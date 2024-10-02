87f7bfc61b42bcf5feb33082e6c5dd028da76222
osin <vincop234@gmail.com>
2024-10-02 16:59:18 +0700

    Modify scripts

--- robot_frame/scripts/run_gazebo_empty_world.sh
+++ robot_frame/scripts/run_gazebo_empty_world.sh
@@ -2,10 +2,22 @@
 #
 # Run Gazebo with world and without robot_frame
 #
-PROJECT_ROOT="$(cd ../..; pwd)"
 
 which gazebo &> /dev/null
 if [ $? -eq 1 ]; then echo "Gazebo not installed!"; exit 1; fi
+
+source /opt/ros/humble/setup.bash
+if [ -z "${COLCON_PREFIX_PATH}" ]; then 
+    source ~/.bashrc
+else
+    source ${COLCON_PREFIX_PATH}/setup.bash; 
+fi
+
+PACKAGE_NAME="robot_frame"
+PROJECT_ROOT=$(source /usr/share/colcon_cd/function/colcon_cd.sh && colcon_cd ${PACKAGE_NAME} > /dev/null && pwd)
+if [ -z "${PROJECT_ROOT}" ]; then 
+    echo "Unable to get root '${PACKAGE_NAME}' package folder..."; exit 1
+fi
 
 PROJECT_MODELS_PATH="${PROJECT_ROOT}/worlds/models:${PROJECT_ROOT}/robot_frame/description/resources"
 echo "Rewrite variable GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${PROJECT_MODELS_PATH}"
